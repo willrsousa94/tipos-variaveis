6094dbdb75aab63092d935437d410cb3797a52c1
William Sousa <williamrsousa94@hotmail.com>
2023-09-03 19:30:07 -0300

    addition of new informations for study and tests of TiposPrimitivos.

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,27 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        /*
+         * Estudar
+         * tipós primitidos
+         * String
+         */
+
+         String meuNome = "William Sousa";
+
+         double salarioMinimo = 1150;
+
+         short numeroCurto = 1;
+         int numeroNormal = numeroCurto;
+         short numeroCurto2 = (short) numeroNormal;
+
+         int numero = 5;
+
+         numero = 10;
+
+         System.out.print(numero);
+
+         final double VALOR_DE_PI = 3.14;
     }
 }
